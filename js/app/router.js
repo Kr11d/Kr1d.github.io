45faaabf4a2212679c012ecee31c273f782c151c
Kr11d <166440950+Kr11d@users.noreply.github.com>
2025-01-29 14:22:34 +0200

    Update router.js

--- js/app/router.js
+++ js/app/router.js
@@ -4,13 +4,5 @@ export const router = VueRouter.createRouter({
     history: VueRouter.createWebHashHistory(),
     routes: [
         { path: '/', name: 'Sign in', component: login },
-        { path: '/campaigns', name: 'Campaigns', component: campaigns },
-        { path: '/campaign/:id', name: 'Campaign', component: campaign },
-        { path: '/users', name: 'Users', component: users },
-        { path: '/user/:id', name: 'User', component: user },
-        { path: '/ads', name: 'Ads', component: ads },
-        { path: '/statistics', name: 'Statistics', component: statistics },
-        { path: '/payments', name: 'Payments', component: payments },
-        { path: '/sites', name: 'Sites', component: sites }, 
     ]
 });
