f8e3dea3b05368378cab87628b60ef8e03a0a8c0
Kr11d <166440950+Kr11d@users.noreply.github.com>
2025-01-29 14:16:48 +0200

    Update router.js

--- js/app/router.js
+++ js/app/router.js
@@ -1,13 +1,4 @@
 import { login } from './pages/login.js';
-import { campaigns } from './pages/campaigns.js';
-import { campaign } from './pages/campaign.js';
-import { users } from './pages/users.js';
-import { user } from './pages/user.js';
-
-import { ads } from './pages/ads.js';
-import { statistics } from './pages/statistics.js';
-import { payments } from './pages/payments.js';
-import { sites } from './pages/sites.js';
 
 export const router = VueRouter.createRouter({
     history: VueRouter.createWebHashHistory(),
